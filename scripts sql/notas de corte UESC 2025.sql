5b850727e302bc94e9ec664725bb769489c3b4d1
Lizandra Amaryllis <monarcasemtempo@gmail.com>
2025-11-20 12:21:32 -0300

    Implementação scripts para UESC

---
+++ scripts sql/notas de corte UESC 2025.sql
@@ -0,0 +1,248 @@
+INSERT INTO CotaCurso (idCota, idCurso, Nota) VALUES
+-- Administração Bacharelado Matutino (idCurso: 1)
+(1, 1, 664.35),
+(2, 1, 658.60),
+(3, 1, 592.97),
+(4, 1, 565.41),
+
+-- Administração Bacharelado Noturno (idCurso: 2)
+(1, 2, 649.59),
+(2, 2, 635.35),
+(3, 2, 606.97),
+(4, 2, 573.35),
+
+-- Agronomia Bacharelado Integral (idCurso: 3)
+(1, 3, 628.82),
+(2, 3, 609.67),
+(3, 3, 563.58),
+(4, 3, 553.75),
+
+-- Biomedicina Bacharelado Integral (idCurso: 4)
+(1, 4, 709.32),
+(2, 4, 693.51),
+(3, 4, 659.11),
+(4, 4, 652.21),
+
+-- Ciência da Computação Bacharelado Integral (idCurso: 5)
+(1, 5, 743.64),
+(2, 5, 720.21),
+(3, 5, 654.54),
+(4, 5, 507.32),
+
+-- Ciências Biológicas Bacharelado Integral (idCurso: 6)
+(1, 6, 660.61),
+(2, 6, 672.85),
+(3, 6, 590.82),
+(4, 6, 545.15),
+
+-- Ciências Biológicas Licenciatura Integral (idCurso: 7)
+(1, 7, 625.43),
+(2, 7, 619.81),
+(3, 7, 550.91),
+(4, 7, 634.44),
+
+-- Ciências Biológicas Licenciatura Noturno (idCurso: 8)
+(1, 8, 606.31),
+(2, 8, 609.92),
+(3, 8, 574.86),
+(4, 8, 526.16),
+
+-- Ciências Contábeis Bacharelado Noturno (idCurso: 9)
+(1, 9, 666.10),
+(2, 9, 635.98),
+(3, 9, 605.87),
+(4, 9, 563.51),
+
+-- Ciências Econômicas Bacharelado Matutino (idCurso: 10)
+(1, 10, 627.56),
+(2, 10, 627.09),
+(3, 10, 558.99),
+(4, 10, 527.37),
+
+-- Ciências Econômicas Bacharelado Noturno (idCurso: 11)
+(1, 11, 621.17),
+(2, 11, 615.77),
+(3, 11, 570.31),
+(4, 11, 502.31),
+
+-- Ciências Sociais Licenciatura Noturno (idCurso: 12)
+(1, 12, 569.58),
+(2, 12, 580.75),
+(3, 12, 551.07),
+(4, 12, 435.37),
+
+-- Comunicação Social - Rádio e Televisão Bacharelado Vespertino (idCurso: 13)
+(1, 13, 658.13),
+(2, 13, 640.00),
+(3, 13, 579.18),
+(4, 13, 565.25),
+
+-- Direito Bacharelado Integral (idCurso: 14)
+(1, 14, 732.90),
+(2, 14, 703.42),
+(3, 14, 660.11),
+(4, 14, 639.69),
+
+-- Direito Bacharelado Noturno (idCurso: 15)
+(1, 15, 723.56),
+(2, 15, 691.69),
+(3, 15, 662.21),
+(4, 15, 636.09),
+
+-- Educação Física Licenciatura Integral (idCurso: 16)
+(1, 16, 625.06),
+(2, 16, 610.47),
+(3, 16, 585.16),
+(4, 16, 487.64),
+
+-- Enfermagem Bacharelado Integral (idCurso: 17)
+(1, 17, 696.64),
+(2, 17, 686.30),
+(3, 17, 642.36),
+(4, 17, 596.89),
+
+-- Engenharia Civil Bacharelado Integral (idCurso: 18)
+(1, 18, 698.34),
+(2, 18, 682.39),
+(3, 18, 626.96),
+(4, 18, 508.94),
+
+-- Engenharia de Produção Bacharelado Integral (idCurso: 19)
+(1, 19, 643.76),
+(2, 19, 620.36),
+(3, 19, 559.68),
+(4, 19, 486.08),
+
+-- Engenharia Elétrica Bacharelado Integral (idCurso: 20)
+(1, 20, 650.43),
+(2, 20, 633.07),
+(3, 20, 578.78),
+(4, 20, 457.41),
+
+-- Engenharia Mecânica Bacharelado Integral (idCurso: 21)
+(1, 21, 673.10),
+(2, 21, 638.71),
+(3, 21, 566.33),
+(4, 21, 468.02),
+
+-- Engenharia Química Bacharelado Integral (idCurso: 22)
+(1, 22, 657.80),
+(2, 22, 587.96),
+(3, 22, 517.71),
+
+-- Filosofia Licenciatura Noturno (idCurso: 23)
+(1, 23, 556.73),
+(2, 23, 497.46),
+(3, 23, 498.23),
+
+-- Filosofia Licenciatura Vespertino (idCurso: 24)
+(1, 24, 551.49),
+(3, 24, 518.19),
+
+-- Física Bacharelado Integral (idCurso: 25)
+(1, 25, 562.77),
+(2, 25, 548.96),
+(3, 25, 407.86),
+(4, 25, 412.74),
+
+-- Física Licenciatura Noturno (idCurso: 26)
+(1, 26, 573.22),
+(2, 26, 545.88),
+(3, 26, 510.55),
+(4, 26, 524.47),
+
+-- Geografia Bacharelado Integral (idCurso: 27)
+(1, 27, 549.66),
+(2, 27, 553.64),
+(3, 27, 523.75),
+(4, 27, 500.50),
+
+-- Geografia Licenciatura Noturno (idCurso: 28)
+(1, 28, 568.26),
+(2, 28, 554.75),
+(3, 28, 558.89),
+(4, 28, 500.91),
+
+-- História Licenciatura Matutino (idCurso: 29)
+(1, 29, 623.65),
+(2, 29, 630.53),
+(3, 29, 582.33),
+(4, 29, 536.96),
+
+-- História Licenciatura Noturno (idCurso: 30)
+(1, 30, 601.57),
+(2, 30, 582.73),
+(3, 30, 570.64),
+(4, 30, 581.20),
+
+-- Letras - Português e Espanhol Licenciatura Matutino (idCurso: 31)
+(1, 31, 598.16),
+(2, 31, 588.54),
+(3, 31, 538.06),
+
+-- Letras - Português e Espanhol Licenciatura Noturno (idCurso: 32)
+(1, 32, 615.49),
+(2, 32, 661.94),
+(3, 32, 558.32),
+
+-- Letras - Português e Inglês Licenciatura Matutino (idCurso: 33)
+(1, 33, 622.32),
+(2, 33, 665.21),
+(3, 33, 591.23),
+(4, 33, 500.11),
+
+-- Letras - Português e Inglês Licenciatura Noturno (idCurso: 34)
+(1, 34, 617.14),
+(2, 34, 669.32),
+(3, 34, 609.26),
+
+-- Línguas Estrangeiras Aplicadas às Negociações Internacionais Bacharelado Vespertino (idCurso: 35)
+(1, 35, 695.84),
+(2, 35, 674.75),
+(3, 35, 627.12),
+(4, 35, 572.28),
+
+-- Matemática Bacharelado Integral (idCurso: 36)
+(1, 36, 565.11),
+(2, 36, 526.19),
+(3, 36, 405.93),
+
+-- Matemática Licenciatura Noturno (idCurso: 37)
+(1, 37, 596.20),
+(2, 37, 582.73),
+(3, 37, 547.08),
+
+-- Medicina Bacharelado Integral (idCurso: 38)
+(1, 38, 792.82),
+(2, 38, 780.47),
+(3, 38, 743.33),
+(4, 38, 716.43),
+
+-- Medicina Veterinária Bacharelado Integral (idCurso: 39)
+(1, 39, 691.31),
+(2, 39, 678.16),
+(3, 39, 630.75),
+(4, 39, 624.80),
+
+-- Pedagogia - Educação Infantil e Anos Iniciais do Ensino Fundamental Licenciatura Matutino (idCurso: 40)
+(1, 40, 594.86),
+(2, 40, 601.55),
+(3, 40, 572.89),
+(4, 40, 532.21),
+
+-- Pedagogia - Educação Infantil e Anos Iniciais do Ensino Fundamental Licenciatura Noturno (idCurso: 41)
+(1, 41, 591.00),
+(2, 41, 605.87),
+(3, 41, 576.64),
+(4, 41, 557.60),
+
+-- Química Bacharelado Integral (idCurso: 42)
+(1, 42, 587.94),
+(2, 42, 597.72),
+(3, 42, 494.62),
+(4, 42, 466.26),
+
+-- Química Licenciatura Integral (idCurso: 43)
+(1, 43, 527.96),
+(2, 43, 517.30),
+(3, 43, 461.53);
